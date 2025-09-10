7f69cd88c2c294cdedf5e553e982dd629daac28a
Kamil Kosek <kamil@kosek.info>
2025-09-10 11:54:51 +0200

    refactor: replace TouchableOpacity with FocusableItem in CurrentSeries component

--- components/series/CurrentSeries.tsx
+++ components/series/CurrentSeries.tsx
@@ -3,9 +3,10 @@ import { router } from "expo-router";
 import { useAtom } from "jotai";
 import type React from "react";
 import { useTranslation } from "react-i18next";
-import { TouchableOpacity, View, type ViewProps } from "react-native";
+import { View, type ViewProps } from "react-native";
 import { apiAtom } from "@/providers/JellyfinProvider";
 import { getPrimaryImageUrlById } from "@/utils/jellyfin/image/getPrimaryImageUrlById";
+import { FocusableItem } from "../common/FocusableItem";
 import { HorizontalScroll } from "../common/HorizontalScroll";
 import { Text } from "../common/Text";
 import Poster from "../posters/Poster";
@@ -27,7 +28,7 @@ export const CurrentSeries: React.FC<Props> = ({ item, ...props }) => {
         data={[item]}
         height={247}
         renderItem={(item, _index) => (
-          <TouchableOpacity
+          <FocusableItem
             key={item?.Id}
             onPress={() =>
               item?.SeriesId && router.push(`/series/${item.SeriesId}`)
@@ -39,7 +40,7 @@ export const CurrentSeries: React.FC<Props> = ({ item, ...props }) => {
               url={getPrimaryImageUrlById({ api, id: item?.ParentId })}
             />
             <Text>{item?.SeriesName}</Text>
-          </TouchableOpacity>
+          </FocusableItem>
         )}
       />
     </View>
